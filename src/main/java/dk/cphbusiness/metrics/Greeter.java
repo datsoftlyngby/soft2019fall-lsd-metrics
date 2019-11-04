69a10c11693aee85dc0d8451532b5b6bca2602e5
eguahlak <anders@kalhauge.dk>
2019-11-04 10:46:39 +0100

    pmd

--- src/main/java/dk/cphbusiness/metrics/Greeter.java
+++ src/main/java/dk/cphbusiness/metrics/Greeter.java
@@ -1,6 +1,10 @@
 package dk.cphbusiness.metrics;
 
 public class Greeter {
+  private int abc;
+  
+  private String ip = "127.0.0.1";
+  
   public String sayHell(String name) {
     StringBuilder result = new StringBuilder();
     if (name == null || name.trim().isEmpty() ) {
@@ -12,4 +16,13 @@ public class Greeter {
     return result.toString();
     }
   
-}
+  public void test() {
+    String[] field = {"a", "b", "c", "s", "e"};
+    String s = "";
+    for (int i = 0; i < field.length; ++i) {
+      s = s + field[i];
+      }
+    System.out.println(ip);
+    }
+  
+  }
